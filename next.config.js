ddbc4d614371ac8d9fe51d316312849605f1e0c8
372768498 <372768498@qq.com>
2025-10-28 15:20:44 +0800

    refactor: streamline architecture - remove redundant microsite routes and use Next.js rewrites

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/microsite/yoyicare',
+        destination: '/microsite-yoyicare.html',
+      },
+      {
+        source: '/microsite/shiningcrystal',
+        destination: '/microsite-shiningcrystal.html',
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
